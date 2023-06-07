9f57d5cdd72979b92440ed002fd4d841683df4b5
Juan Rodriguez <75664608+sowkee@users.noreply.github.com>
2023-06-07 17:22:54 -0500

    update

--- src/main/java/com/credibanco/client/Consumir/HttpService.java
+++ src/main/java/com/credibanco/client/Consumir/HttpService.java
@@ -1,34 +1,37 @@
 package com.credibanco.client.Consumir;
 
-import com.credibanco.client.DTO.UserDto;
-
+import com.credibanco.dependencia.DTO.RequestDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
+import static com.credibanco.dependencia.Http.HttpService.getHttpEntity;
+
+
 public class HttpService {
     private static final Logger logger = LoggerFactory.getLogger(HttpService.class);
+    @Autowired
+    static RequestDTO requestDTO;
+
     public static String consumirService(){
-        UserDto userDto = new UserDto();
-        userDto.setId(2L);
-        userDto.setUserName("Juan");
-        userDto.setUserPassword("12345");
-        ResponseEntity<String> response = new RestTemplate().exchange("http://localhost:8081/users/create", HttpMethod.POST, getHttpEntity(userDto), String.class);
-        if (response.getStatusCode() == HttpStatus.OK) {
-            logger.info(response.getBody());
+        try {
+            logger.info("-------Envia la peticion-------");
+
+            requestDTO.setId(200);
+            requestDTO.setUserName("Juan");
+            requestDTO.setUserPassword("12345");
+
+            ResponseEntity<String> response = new RestTemplate().exchange("http://localhost:8081/users/create", HttpMethod.POST, getHttpEntity(requestDTO), String.class);
+            if (response.getStatusCode() == HttpStatus.OK) {
+                logger.info(response.getBody());
+            }
+        } catch (Exception e) {
+            logger.error("------Error------", e);
         }
         return null;
     }
 
-    private static HttpEntity getHttpEntity(UserDto userDto) {
-        return new HttpEntity(userDto, getHttpHeadersJson());
-    }
 
-    private static HttpHeaders  getHttpHeadersJson(){
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-type", "application/json");
-        headers.add("Accept", "application/json");
-        return headers;
-    }
 }
